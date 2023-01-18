b17748663e036e5bedd7c128ababa73d0ec28879
4lorelei <43756481+4lorelei@users.noreply.github.com>
2023-01-18 17:00:55 +0100

    Add files via upload

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/web-app-python.herokuapp.com/')
 def hello():
     return jsonify(message='Hello, World!')
 
