6f5bfdce244a1864945f73168ca9a258e0c634dc
4lorelei <43756481+4lorelei@users.noreply.github.com>
2023-01-18 14:31:10 +0100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+@app.route('/greet', methods=['POST'])
+def greet():
+    name = request.form['name']
+    return f'Hello, {name}!'
+
+if __name__ == '__main__':
+    app.run()
