e3777343899d5778942807f52278bcb2d36d9ae4
4lorelei <43756481+4lorelei@users.noreply.github.com>
2023-01-18 16:32:24 +0100

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,14 +1,10 @@
-from flask import Flask, request
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
-
-@app.route('/greet', methods=['POST'])
-def greet():
-    name = request.form['name']
-    return f'Hello, {name}!'
+    return jsonify(message='Hello, World!')
 
 if __name__ == '__main__':
     app.run()
